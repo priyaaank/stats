a6165b0ce736312559494bedd5f2b2a74a77eaa7
Priyank Gupta <priyaaank@gmail.com>
2012-03-24 16:52:23 -0400

    Added basic code to fetch queue depth. Now adding all the jazz to give it shiny look

--- web.rb
+++ web.rb
@@ -1,21 +1,24 @@
-require 'yaml'
-require 'right_aws'
-require 'sinatra/base'
-
+['yaml', 'right_aws', 'sinatra/base', 'json'].each {|r| require r }
 Dir["mixins/**/*.rb"].sort.each {|file| require_relative file if file.include?(".rb") }
 Dir["models/**/*.rb"].sort.each {|file| require_relative file if file.include?(".rb") }
 
 class Web < Sinatra::Base
 
   get '/' do
-    "Dragons be here!"
+    erb :base
   end
 
-  get '/sqs' do
-    queues = Queue.new.list
-    names = queues.map(&:name).join(",")
+  get '/sqs.json' do
+    content_type :json
     status 200
-    body names
+    queue_data_hash.to_json
+  end
+
+  private
+
+  def queue_data_hash
+    queues = Queue.new.list
+    queues.inject({}) {|hash,queue| hash[queue.name] = queue.size; hash}
   end
 
 end
